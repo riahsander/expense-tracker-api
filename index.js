11097e529e0c6b3b10c097b4549ca3d11e80ac1f
Riah Sander <riahsander05@gmail.com>
2026-09-24 14:47:31 -0300

    feat: adding express-rate-limit

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const setupSwagger = require("./src/swagger/swaggerConfig"); // Importa a config
 
 const app = express();
 
+const rateLimit = require("express-rate-limit");
+
+// Configuração do Limite de Requisições
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // Janela de tempo: 15 minutos
+  max: 100, // Limite máximo: cada IP pode fazer até 100 requisições por janela de 15 minutos
+  standardHeaders: true, // Retorna informações de limite nos headers de resposta (RateLimit-*)
+  legacyHeaders: false, // Desativa os headers antigos (X-RateLimit-*)
+  message: {
+    status: 429,
+    error:
+      "Muitas requisições feitas a partir deste IP, por favor tente novamente mais tarde.",
+  },
+});
+
+// Aplicar o limitador globalmente em todas as rotas da API
+app.use(limiter);
+
 app.use(express.json());
 app.use(cors());
 
